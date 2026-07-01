d98036b7182b31be506a818bc1601dad6fb7c455
Markshep <markshep@yandex.ru>
2026-07-01 10:56:58 +0300

    Main Menu MVP update

--- src/client/java/ru/markshep/clippy_but_something_wrong/client/animations/clippy/RandomAnimation.java
+++ src/client/java/ru/markshep/clippy_but_something_wrong/client/animations/clippy/RandomAnimation.java
@@ -13,50 +13,29 @@ public class RandomAnimation {
 
     public static Statement randomEventAnimation(){
         int percent = random.nextInt(100) + 1;
-        if (percent == 1) {
-            return Statement.SLEEP;
-        } else if (percent == 2) {
-            return Statement.WTF;
-        } else if (percent == 3) {
-            return Statement.POINT_DOWN;
-        } else if (percent == 4) {
-            return Statement.POINT_UP;
-        } else if (percent == 5) {
-            return Statement.POINT_LEFT;
-        } else if (percent == 6) {
-            return Statement.POINT_RIGHT;
-        } else if (percent == 7) {
-            return Statement.WATCH_FRONT;
-        } else if (percent == 8) {
-            return Statement.WATCH_LEFT;
-        } else if (percent == 9) {
-            return Statement.WATCH_RIGHT;
-        } else if (percent == 10) {
-            return Statement.THINK;
-        } else if (percent == 11) {
-            return Statement.SHOVEL;
-        } else if (percent == 12) {
-            return Statement.OFFICE;
-        } else if (percent == 13) {
-            return Statement.TIRED;
-        } else if (percent == 14) {
-            return Statement.SPYGLASS;
-        } else if (percent == 15) {
-            return Statement.AIRPLANE;
-        } else if (percent == 16) {
-            return Statement.CHECK;
-        } else if (percent == 17) {
-            return Statement.EYES;
-        } else if (percent == 18) {
-            return Statement.LISTEN;
-        } else if (percent == 19) {
-            return Statement.MUSIC;
-        } else if (percent == 20) {
-            return Statement.NOTED;
-        } else if (percent == 21) {
-            return Statement.READ;
-        } else {
-            return Statement.IDLE;
+        switch (percent) {
+            case 1: return Statement.SLEEP;
+            case 2: return Statement.WTF;
+            case 3: return Statement.POINT_DOWN;
+            case 4: return Statement.POINT_UP;
+            case 5: return Statement.POINT_LEFT;
+            case 6: return Statement.POINT_RIGHT;
+            case 7: return Statement.WATCH_FRONT;
+            case 8: return Statement.WATCH_LEFT;
+            case 9: return Statement.WATCH_RIGHT;
+            case 10: return Statement.THINK;
+            case 11: return Statement.SHOVEL;
+            case 12: return Statement.OFFICE;
+            case 13: return Statement.TIRED;
+            case 14: return Statement.SPYGLASS;
+            case 15: return Statement.AIRPLANE;
+            case 16: return Statement.CHECK;
+            case 17: return Statement.EYES;
+            case 18: return Statement.LISTEN;
+            case 19: return Statement.MUSIC;
+            case 20: return Statement.NOTED;
+            case 21: return Statement.READ;
+            default: return Statement.IDLE;
         }
     }
 
